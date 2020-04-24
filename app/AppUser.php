e7204fc6d624feda2786b930cdc5849f8874ae86
momit.litu <momit.litu@gmail.com>
2020-04-24 12:34:35 +0600

    first pyush after handover from  siam

--- app/AppUser.php
+++ app/AppUser.php
@@ -11,4 +11,33 @@ class AppUser extends Model
     protected $fillable = [
         'name','nid_no','email','contact_no','status','address','password','remarks', 'user_profile_image',
     ];
+	
+	/**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+	
+	    /**
+     * Change login status according to $status.
+     *
+     * @param string $status
+     * @return mixed
+     */
+
+    public static function LogInStatusUpdate($status)
+    {
+        if(\Auth::check()){
+            if($status=='login') {
+                $change_status=1;
+            } else {
+                $change_status=0;
+            }
+            $loginstatuschange = \App\AppUser::where('email',\Auth::customer()->email)->update(array('login_status'=>$change_status));
+            return $loginstatuschange;
+        }
+    }
 }
--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+		
+		'customer'  => [
+          'driver'  => 'session',
+          'provider' => 'customers',
+        ],
 
         'api' => [
             'driver' => 'token',
@@ -70,7 +75,11 @@ return [
             'driver' => 'eloquent',
             'model' => App\User::class,
         ],
-
+		
+        'customers' => [
+            'driver' => 'eloquent',
+            'model' => App\AppUser::class,
+        ],
         // 'users' => [
         //     'driver' => 'database',
         //     'table' => 'users',
@@ -95,6 +104,12 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
+            'table' => 'password_resets',
+            'expire' => 60,
+            'throttle' => 60,
+        ],
+		'customers' => [
+            'provider' => 'customers',
             'table' => 'password_resets',
             'expire' => 60,
             'throttle' => 60,
