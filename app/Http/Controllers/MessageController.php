b6753c9b7c0cdcfc96857ae783f8b599b6351477
Prosanta Kumar Chaki <kajolchaki@gmail.com>
2020-04-27 11:24:11 +0600

    message top partially done

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -584,12 +584,46 @@ class MessageController extends Controller
     }
 
     public function newMessageLoad(){
-        $message = DB::table('message_masters as mm')
+        $groupMessage = DB::table('message_masters as mm')
+            ->leftJoin('app_users as apu', 'mm.app_user_id', '=', 'apu.id')
+            ->leftJoin('message_categories as mc', 'mm.message_category', '=', 'mc.id')
+            ->leftJoin('user_groups as ug', 'mm.group_id', '=', 'ug.id')
+            ->where('mm.is_seen',0)
+            ->where('mm.is_group_msg',1)
+            ->whereNull('admin_message')
+            ->select('mm.id as id', 'mm.app_user_id as app_user_id', 'mm.app_user_message as app_user_message', 'mm.admin_id as admin_id', 'mm.admin_message as admin_message','mm.created_at as msg_date', 'mm.is_attachment as is_attachment', 'mm.admin_id as admin_id', 'mm.is_attachment_app_user as is_attachment_app_user', 'mc.category_name as category_name', 'ug.group_name as group_name')
+            ->groupBy('mm.group_id', 'mm.message_category')
+            ->orderBy('mm.created_at', 'desc')
+            ->get();
+
+        $individualMessage = DB::table('message_masters as mm')
             ->leftJoin('app_users as apu', 'mm.app_user_id', '=', 'apu.id')
             ->leftJoin('message_categories as mc', 'mm.message_category', '=', 'mc.id')
             ->where('mm.is_seen',0)
-            ->select('mm.id as id', 'mm.app_user_id as app_user_id', 'mm.app_user_message as app_user_message', 'mm.admin_id as admin_id', 'mm.admin_message as admin_message','mm.created_at as msg_date', 'mm.is_attachment as is_attachment', 'mm.admin_id as admin_id', 'mm.is_attachment_app_user as is_attachment_app_user')
+            ->where('mm.is_group_msg',0)
+            ->whereNull('admin_message')
+            ->select('mm.id as id', 'mm.app_user_id as app_user_id', 'mm.app_user_message as app_user_message', 'mm.admin_id as admin_id', 'mm.admin_message as admin_message','mm.created_at as msg_date', 'mm.is_attachment as is_attachment', 'mm.admin_id as admin_id', 'mm.is_attachment_app_user as is_attachment_app_user', 'mc.category_name as category_name','apu.name as app_user_name')
+            ->groupBy('mm.app_user_id')
+            ->orderBy('mm.created_at', 'desc')
             ->get();
+        foreach ($individualMessage as $value){
+            $newMessage[strtotime($value->msg_date)]=$value;
+        }
+        foreach ($groupMessage as $value){
+            $newMessage[strtotime($value->msg_date)]=$value;
+        }
+
+        usort($newMessage, function($a, $b)
+        {
+            if ($a == $b)
+                return (0);
+            return (($a < $b) ? -1 : 1);
+        });
+
+        //$message = (object) array_merge((array) $groupMessage, (array) $individualMessage);
+
+        return json_encode($newMessage);
+
     }
 
 
